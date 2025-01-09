8e0319557d268ba3c0e81a409358aaf7c20ab429
Snehithaaax <152869534+Snehithaaax@users.noreply.github.com>
2025-01-09 10:46:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+
+// let Name ="Sahithya";
+// let _a = "snehitha"
+// console.log(_a);
+// var a = 1;
+// var a=5;
+// document.write(a+a);
+// console.log(a+a);
+// let b = 20;
+//  b = 70
+// console.log(b+b);
+// var l = "sai"
+// {
+//     var l = "kumar"
+//     console.log(l);
+// }
+//console.log(l);
+// let a = "sai";
+// {
+//    let a ="kumar"
+//      console.log(a);
+//     console.log(a);
+// }
+// console.log(a)
+// let a = 1
+// console.log("hi +a+a")
+// document.write('hi+ a+a' , "<br>") 
+// console.log(`hi+${a+a}`);
+// document.write(`hi+${a+a}`);
+// let x = "sai";
+// let y = "kumar";
+// // let age = 30;
+// // let c = `I am ${x+" "+ y} , my age is ${age} years old`
+// // console.log(c);
+// console.log(`Reactjs Student = ${x+" "+y}`);
+// document.write(`Reactjs Student = ${x+" "+y}`);
+let a = null;
+document.write(a);
+document.write(type(a))
